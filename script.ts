92d63ab47ee204a29478337a01dd9df77f63318c
Yuuki Kanasugi <knsg16@gmail.com>
2021-02-21 13:32:55 +0900

    created post

--- script.ts
+++ script.ts
@@ -5,10 +5,20 @@ const prisma = new PrismaClient()
 // A `main` function so that you can use async/await
 async function main() {
   // ... you will write your Prisma Client queries here
+  const post = await prisma.post.create({
+    data: {
+      title: "hoge",
+      author: {
+        connect: { email: 'sarah@prisma.io' },
+      },
+    },
+  })
+  console.log(post)
+
   const allUsers = await prisma.user.findMany({
     include: { posts: true },
-  });
-  console.dir(allUsers, { depth: null });
+  })
+  console.dir(allUsers, { depth: null })
 }
 
 main()
